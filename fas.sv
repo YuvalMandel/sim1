3c50d3d888e63eefb99402562b993f24be589c7e
Yuval Mandel <tubal.mandel.212@gmail.com>
2019-12-19 14:45:22 +0200

    ny latets version

--- fas.sv
+++ fas.sv
@@ -8,10 +8,64 @@ module fas (
     output logic cout        // Carry out
 );
 
-// Put your code here
-// ------------------
+	parameter nand_tpd = 1;
+	parameter or_tpd = 1;
+	parameter xnor_tpd = 1;
 
-
-// End of your code
+	logic d;
+	logic e;
+	logic f;
+	logic g;
+	logic h;
+	
+	logic b_xnor_cin;
+	
+	// This is the logic for cout
+	NAND2 #(
+		.Tpdlh(nand_tpd),
+		.Tpdhl(nand_tpd)
+	) NAND2_instance (
+		.A(b),
+		.B(cin),
+		.Z(d)
+	);
+	
+	
+	OR2 #(
+		.Tpdlh(or_tpd),
+		.Tpdhl(or_tpd)
+	) OR2_instance (
+		.A(b),
+		.B(cin),
+		.Z(e)
+	);
+	
+	XNOR2 #(
+		.Tpdlh(xnor_tpd),
+		.Tpdhl(xnor_tpd)
+	) XNOR2_instance (
+		.A(a),
+		.B(a_ns),
+		.Z(f)
+	);
+	
+	NAND2 #(
+		.Tpdlh(nand_tpd),
+		.Tpdhl(nand_tpd)
+	) NAND2_Binstance (
+		.A(e),
+		.B(f),
+		.Z(g)
+	);
+	
+	NAND2 #(
+		.Tpdlh(nand_tpd),
+		.Tpdhl(nand_tpd)
+	) NAND2_Cinstance (
+		.A(d),
+		.B(g),
+		.Z(Z)
+	);
+	// This is the logic for s
 
 endmodule
--- fas_test.sv
+++ fas_test.sv
@@ -1,10 +1,71 @@
 // Full Adder/Subtractor test bench template
 module fas_test;
 
-// Put your code here
-// ------------------
+	logic a; // Input bit a
+	logic b; // Input bit b
+	logic cin; // Carry in
+	logic a_ns; // A_nS (add/not subtract) control
+	logic s; // Output S
+	logic cout; // Carry out
+	
+	logic[1:0] result;
 
+	parameter nand_tpd = 0;
+	parameter or_tpd = 0;
+	parameter xnor_tpd = 0;
 
-// End of your code
+	fas #(
+		.nand_tpd(nand_tpd),
+		.or_tpd(or_tpd),
+		.xnor_tpd(xnor_tpd)
+	) fas_instance (
+		.a(a),
+		.b(b),
+		.cin(cin),
+		.a_ns(a_ns),
+		.s(s),
+		.cout(cout)
+	);
+	
+	always_comb begin
+		result = {cout, s};
+	end
+
+	initial begin
+
+		// set all parameters for shortest time.
+		a = 1'b0;
+		b = 1'b0;
+		cin = 1'b0;
+		a_ns = 1'b0;
+		
+		#40ns;
+	
+		for(int i = 0; i < 2; i++) begin
+			
+			a_ns <= !a_ns;
+		
+			for(int i = 0; i < 2; i++) begin
+		
+				cin <= !cin;
+				
+				for(int i = 0; i < 2; i++) begin
+		
+					b <= !b;
+					
+					for(int i = 0; i < 2; i++) begin
+		
+						a <= !a;
+						#40ns;
+		
+					end
+		
+				end
+		
+			end
+		
+		end
+
+	end;
 
 endmodule
